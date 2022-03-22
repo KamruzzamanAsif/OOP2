646865798bda5354cabfd99aa416af87e9b363b7
asif_1217 <asif720kamruzzaman@gmail.com>
2022-03-22 15:50:12 +0600

    thread

---
+++ OOP2/src/Threading/PrintTask.java
@@ -0,0 +1,25 @@
+package Threading;
+
+import java.util.Random;
+
+public class PrintTask implements Runnable{
+    private final int sleepTime;
+    private final String taskName;
+    private final static Random generator = new Random();
+
+    public PrintTask(String name){
+        taskName = name;
+        sleepTime = generator.nextInt(5000);
+    }
+
+    public void run(){
+        try{
+            System.out.printf("%s is going to sleep for %d milliseconds.\n", taskName, sleepTime);
+            Thread.sleep(sleepTime);
+        }catch (InterruptedException exception){
+            System.out.printf("%s %s\n", taskName, "terminated permanently due to interruption");
+        }
+
+        System.out.printf("%s done sleeping\n", taskName);
+    }
+}
---
+++ OOP2/src/Threading/ThreadCreator.java
@@ -0,0 +1,19 @@
+package Threading;
+
+public class ThreadCreator {
+    public static void main(String[] args) {
+        System.out.printf("Creating threads.\n");
+
+        Thread thread1 = new Thread(new PrintTask("task1"));
+        Thread thread2 = new Thread(new PrintTask("task2"));
+        Thread thread3 = new Thread(new PrintTask("task3"));
+
+        System.out.printf("Threads created, starting tasks.\n");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        System.out.printf("Tasks started, main ends.\n");
+    }
+}
